3d6af12fe0c0f9ba3377ab4b6dd99e8761fa47ac
takker99 <37929109+takker99@users.noreply.github.com>
2021-12-18 09:29:58 +0900

    :sparkles: Implement unknownutil

---
+++ utils.ts
@@ -0,0 +1,27 @@
+// The most of this code is ported from https://deno.land/x/unknownutil@v1.1.0/mod.ts
+import { isValid, parse } from "./deps/date-fns.ts";
+
+export type Predicate<T> = (value: unknown) => value is T;
+export function isNone(value: unknown): value is undefined | null {
+  return value === null;
+}
+export function isString(value: unknown): value is string {
+  return typeof value === "string";
+}
+export function isNumber(value: unknown): value is number {
+  return typeof value === "number";
+}
+export function isArray<T extends unknown>(
+  value: unknown,
+  predicate?: Predicate<T>,
+): value is T[] {
+  return Array.isArray(value) && (!predicate || value.every(predicate));
+}
+
+export function ensureArray<T>(
+  value: unknown,
+  predicate?: Predicate<T>,
+): asserts value is Array<T> {
+  if (!isArray(value, predicate)) return;
+  throw TypeError("value must be an Array");
+}
