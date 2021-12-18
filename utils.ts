3ebf0b25523af92b37227ff757488ed9633e441d
takker99 <37929109+takker99@users.noreply.github.com>
2021-12-18 09:37:18 +0900

    :recycle: getDatesFromSelectionのTS化とiterator化

--- utils.ts
+++ utils.ts
@@ -1,4 +1,5 @@
 // The most of this code is ported from https://deno.land/x/unknownutil@v1.1.0/mod.ts
+import { rangeText } from "./lib/selection.ts";
 import { isValid, parse } from "./deps/date-fns.ts";
 
 export type Predicate<T> = (value: unknown) => value is T;
@@ -24,4 +25,13 @@ export function ensureArray<T>(
 ): asserts value is Array<T> {
   if (!isArray(value, predicate)) return;
   throw TypeError("value must be an Array");
+}
+
+export function* getDatesFromSelection() {
+  const now = new Date();
+  for (const [dateString] of rangeText().matchAll(/\d{4}-\d{2}-\d{2}/g)) {
+    const date = parse(dateString, "yyyy-MM-dd", now, undefined);
+    if (!isValid(date)) continue;
+    yield date;
+  }
 }
