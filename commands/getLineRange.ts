5856ae1cdd3bdab062a1e44f4112caba9b92d3f3
takker99 <37929109+takker99@users.noreply.github.com>
2022-01-17 02:04:16 +0900

    :bug: Rangeは若い番号がstartになるとも限らない

--- commands/getLineRange.ts
+++ commands/getLineRange.ts
@@ -5,5 +5,7 @@ export function getLineRange() {
   const { selectionRange: { start, end }, selectedText, position } = caret();
   return selectedText === ""
     ? [position.line, position.line]
+    : start.line > end.line
+    ? [end.line, start.line]
     : [start.line, end.line] as const;
 }
