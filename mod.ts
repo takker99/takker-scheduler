5e8f55a18c9877d5e055db6da237fa16c3eb5319
takker99 <37929109+takker99@users.noreply.github.com>
2022-02-10 13:13:57 +0900

    :recycle: type checkの対象にcreateTask.tsを含める

--- mod.ts
+++ mod.ts
@@ -3,6 +3,7 @@ export { startTask } from "./commands/startTask.ts";
 export { endTask } from "./commands/endTask.ts";
 export { toggleTask } from "./commands/toggleTask.ts";
 export { posterioriEndTask } from "./commands/posterioriEndTask.ts";
+export { createTask } from "./commands/createTask.ts";
 export { walkDay } from "./commands/walkDay.ts";
 export { moveToday } from "./commands/moveToday.ts";
 export { format } from "./commands/format.ts";
