2ecef339cb4a136141dc3349a74de378102f2984
takker99 <37929109+takker99@users.noreply.github.com>
2022-02-10 12:55:49 +0900

    :bug: pathの置き換え漏れ

--- utils.test.ts
+++ utils.test.ts
@@ -1,35 +1,7 @@
 /// <reference lib="deno.unstable" />
-import { ensureArray, isNumber, isString, oneByOne } from "./utils.ts";
-import { sleep } from "./lib/sleep.ts";
+import { isString, oneByOne } from "./utils.ts";
+import { sleep } from "./deps/scrapbox-std.ts";
 import { assertEquals, assertThrows } from "./deps/testing.ts";
-
-Deno.test("ensureArray()", async ({ step }) => {
-  // ported from https://github.com/lambdalisue/deno-unknownutil/blob/bb05a449578707fde3f26a92973d3ab0a5cdc101/ensure_test.ts#L70
-  await step("does nothing on array", () => {
-    ensureArray([]);
-    ensureArray([0, 1, 2]);
-    ensureArray(["a", "b", "c"]);
-  });
-  await step("throws error on non array", () => {
-    assertThrows(() => ensureArray("a"));
-    assertThrows(() => ensureArray(0));
-    assertThrows(() => ensureArray(true));
-    assertThrows(() => ensureArray(false));
-    assertThrows(() => ensureArray({}));
-    assertThrows(() => ensureArray(function () {}));
-    assertThrows(() => ensureArray(undefined));
-    assertThrows(() => ensureArray(null));
-  });
-  await step("does nothing on T array", () => {
-    ensureArray([0, 1, 2], isNumber);
-    ensureArray(["a", "b", "c"], isString);
-  });
-  await step("throws error on non T array", () => {
-    assertThrows(() => ensureArray([0, 1, 2], isString));
-    assertThrows(() => ensureArray(["a", "b", "c"], isNumber));
-    assertThrows(() => ensureArray([true, false, true], isString));
-  });
-});
 
 Deno.test("oneByOne()", async () => {
   const delay = async (i: number, j: number) => {
