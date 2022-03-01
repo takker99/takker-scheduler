730cbd1487219dc6d090c62a8cc2c25c770b18d3
GitHub <noreply@github.com>
2022-03-01 00:49:00 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox-websocket.ts
+++ deps/scrapbox-websocket.ts
@@ -1,4 +1,4 @@
 export {
   joinPageRoom,
   patch,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.8.5/browser/websocket/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.9.0/browser/websocket/mod.ts";
--- deps/scrapbox.ts
+++ deps/scrapbox.ts
@@ -3,6 +3,6 @@ import type {
   PageList,
   Scrapbox,
 } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.0.8/mod.ts";
-export { getPage } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.8.5/rest/pages.ts";
+export { getPage } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.9.0/rest/pages.ts";
 export type Line = Page["lines"][0];
 export type { PageList, Scrapbox };
