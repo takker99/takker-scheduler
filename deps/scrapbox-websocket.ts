31e81c0f2431e0eafe1794d4425415ae4ac282fd
GitHub <noreply@github.com>
2022-03-26 00:44:12 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox-websocket.ts
+++ deps/scrapbox-websocket.ts
@@ -2,7 +2,7 @@ export {
   disconnect,
   makeSocket,
   patch,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.10.6/browser/websocket/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.11.1/browser/websocket/mod.ts";
 export type {
   Socket,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.10.6/browser/websocket/mod.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.11.1/browser/websocket/mod.ts";
