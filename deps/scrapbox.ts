b1509cd892635be6ee3ebba23c1bad1013a933ac
GitHub <noreply@github.com>
2022-04-25 00:47:42 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox.ts
+++ deps/scrapbox.ts
@@ -3,6 +3,6 @@ import type {
   PageList,
   Scrapbox,
 } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.3.3/mod.ts";
-export { getPage } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.13.0/rest/pages.ts";
+export { getPage } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.1/rest/pages.ts";
 export type Line = Page["lines"][0];
 export type { PageList, Scrapbox };
