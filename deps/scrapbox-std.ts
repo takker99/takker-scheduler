dffcad6f5456fa6cca2d5bc4ff1071579c49501c
GitHub <noreply@github.com>
2022-09-02 01:05:08 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox-std.ts
+++ deps/scrapbox-std.ts
@@ -4,11 +4,11 @@ export type {
 } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.3.6/rest.ts";
 export {
   getIndentLineCount,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.8/text.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.9/text.ts";
 export {
   encodeTitleURI,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.8/title.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.9/title.ts";
 export {
   sleep,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.8/sleep.ts";
-export { getPage } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.8/rest/pages.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.9/sleep.ts";
+export { getPage } from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.14.9/rest/pages.ts";
