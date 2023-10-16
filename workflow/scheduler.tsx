fe2575b9cc823b0e2725b9f5ecf9395622ed7df8
takker <37929109+takker99@users.noreply.github.com>
2023-10-16 10:45:36 +0000

    :bug: viewer.tsxとのCSSの衝突回避

--- workflow/scheduler.tsx
+++ workflow/scheduler.tsx
@@ -127,7 +127,7 @@ const App = ({ getController, projects, mainProject }: Props) => {
           </button>
           <button className="close" onClick={close}>{"\uf00d"}</button>
         </div>
-        <ul className="result" onClick={stopPropagation} data-page-no={pageNo}>
+        <ul className="result scheduler" onClick={stopPropagation} data-page-no={pageNo}>
           {dateList.map((date) => (
             <li key={toKey(date)}>
               <DailySchedule
