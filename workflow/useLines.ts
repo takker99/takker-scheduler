edd527bd191f445442cba1621b8adb30aa2211a4
takker <37929109+takker99@users.noreply.github.com>
2023-10-19 23:11:10 +0000

    :bug: ページ遷移時に、別のページの本文がcacheされてしまうことがあった

--- workflow/useLines.ts
+++ workflow/useLines.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "../deps/preact.tsx";
-import { getPage } from "../deps/scrapbox-std.ts";
+import { getPage, toTitleLc } from "../deps/scrapbox-std.ts";
 import { Scrapbox, takeInternalLines } from "../deps/scrapbox-std-dom.ts";
 declare const scrapbox: Scrapbox;
 
 type Key = `/${string}/${string}`;
-const toKey = (project: string, title: string): Key => `/${project}/${title}`;
+const toKey = (project: string, title: string): Key =>
+  `/${project}/${toTitleLc(title)}`;
 const pages = new Map<Key, string[]>();
 type Listener = (lines: string[]) => void;
 const listeners = new Map<Key, Set<Listener>>();
@@ -15,8 +16,10 @@ const emitChange = (key: Key, lines: string[]) => {
   }
 };
 // 入力するたびに更新する
+// 更新は`get()`で1回以上呼び出されたpagesのみを対象とする
 {
   let update: undefined | (() => void);
+  let timer: undefined | number;
   scrapbox.on("page:changed", () => {
     if (update) scrapbox.off("lines:changed", update);
 
@@ -25,8 +28,24 @@ const emitChange = (key: Key, lines: string[]) => {
     const key = toKey(project, title);
     if (!pages.has(key)) return;
 
-    update = () =>
-      emitChange(key, takeInternalLines().map((line) => line.text));
+    update = () => {
+      if (timer !== undefined) {
+        clearInterval(timer);
+        timer = undefined;
+      }
+      const lines = takeInternalLines().map((line) => line.text);
+
+      // 更新のタイミングの影響で、本文がまだ更新されていない可能性がある
+      // その場合は`title`と本文先頭の文字列が一致するまで待機する
+      timer = setInterval(() => {
+        if (toTitleLc(title) !== toTitleLc(lines[0])) return;
+        emitChange(key, lines);
+        if (timer !== undefined) {
+          clearInterval(timer);
+          timer = undefined;
+        }
+      }, 1000);
+    };
     scrapbox.on("lines:changed", update);
   });
 }
