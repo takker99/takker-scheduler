33f79da061649d2e1775e80fbaad2a4e88ac9989
takker99 <37929109+takker99@users.noreply.github.com>
2023-11-05 16:21:04 +0900

    :recycle: separate `useNavigation`

--- workflow/viewer.tsx
+++ workflow/viewer.tsx
@@ -11,7 +11,6 @@ import {
   useCallback,
   useEffect,
   useMemo,
-  useState,
 } from "../deps/preact.tsx";
 import { useTaskCrawler } from "./useTaskCrawler.ts";
 import { useDialog } from "./useDialog.ts";
@@ -30,9 +29,10 @@ import { getEnd, getStart, Reminder } from "../howm/parse.ts";
 import { Period } from "../howm/Period.ts";
 import { compareFn } from "../howm/sort.ts";
 import { Status } from "../howm/status.ts";
-import { Key, toKey, toLocalDate } from "./key.ts";
+import { Key, toLocalDate } from "./key.ts";
 import { ProgressBar } from "./ProgressBar.tsx";
 import { TaskItem } from "./TaskItem.tsx";
+import { useNavigation } from "./useNavigation.tsx";
 export declare const scrapbox: Scrapbox;
 
 export interface Controller {
@@ -150,52 +150,4 @@ const App = ({ getController, projects }: Props) => {
       </dialog>
     </>
   );
-};
-
-type PageNo = Key | "expired" | "errors";
-
-const useNavigation = (
-  defaultPageNo: PageNo = toKey(new Date()),
-) => {
-  /** 日付の場合は現在表示しているタスクリストの基準点を表す
-   * `expired`のときはやり残した予定を表示する
-   * `error`のときはエラーを表示する
-   */
-  const [pageNo, setPageNo] = useState<PageNo>(defaultPageNo);
-
-  const next = useCallback(() => {
-    setPageNo((pageNo) => {
-      switch (pageNo) {
-        case "errors":
-          return "expired";
-        case "expired":
-          return toKey(new Date());
-        default: {
-          const date = toDate(toLocalDate(pageNo));
-          date.setDate(date.getDate() + 1);
-          return toKey(date);
-        }
-      }
-    });
-  }, []);
-  const prev = useCallback(() => {
-    setPageNo((pageNo) => {
-      const nowKey = toKey(new Date());
-      switch (pageNo) {
-        case "errors":
-          return "errors";
-        case "expired":
-          return "errors";
-        case nowKey:
-          return "expired";
-        default: {
-          const date = toDate(toLocalDate(pageNo));
-          date.setDate(date.getDate() - 1);
-          return toKey(date);
-        }
-      }
-    });
-  }, []);
-
-  return { pageNo, next, prev };
 };
