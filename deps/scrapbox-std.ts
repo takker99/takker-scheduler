31a84b33bd4c07c56e7d4580ebea0c4bd4a6a832
GitHub <noreply@github.com>
2024-05-26 00:51:07 +0000

    :arrow_up: update deno dependencies

--- deps/scrapbox-std.ts
+++ deps/scrapbox-std.ts
@@ -4,13 +4,13 @@ export type {
 } from "https://raw.githubusercontent.com/scrapbox-jp/types/0.7.1/rest.ts";
 export {
   getIndentLineCount,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.25.1/text.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.0/text.ts";
 export {
   encodeTitleURI,
   toTitleLc,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.25.1/title.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.0/title.ts";
 export {
   sleep,
-} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.25.1/sleep.ts";
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.25.1/rest/mod.ts";
-export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.25.1/is.ts";
+} from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.0/sleep.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.0/rest/mod.ts";
+export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.26.0/is.ts";
