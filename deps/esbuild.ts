8ded21a50f75af58f741160e711b90b21e5896c6
GitHub <noreply@github.com>
2024-07-01 00:55:03 +0000

    :arrow_up: update deno dependencies

--- deps/esbuild.ts
+++ deps/esbuild.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/esbuild@v0.21.5/mod.js";
+export * from "https://deno.land/x/esbuild@v0.22.0/mod.js";
 export * from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
